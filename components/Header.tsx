8c5f0d75a549f7dd13e464ee50933811689e4b92
patetisho1 <patetisho1@gmail.com>
2026-02-04 16:41:25 +0200

    fix: add missing Link import in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { Menu, X, User, LogOut, Bell, ChevronDown, ArrowRight } from 'lucide-react'
 import ThemeToggle from './ThemeToggle'
